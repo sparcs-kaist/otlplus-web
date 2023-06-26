36584593aa16eb96e7a227a1d0d6ca0219f85462
zealotzealot <hsh0908y@gmail.com>
2023-06-26 19:31:36 +0900

    feat: implement and apply PlannerOverlay for course addition

---
+++ src/components/PlannerOverlay.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+
+import { appBoundClassNames as classNames } from '../common/boundClassNames';
+import { getSemesterName } from '../utils/semesterUtils';
+
+const PlannerOverlay = ({
+  t,
+  yearIndex,
+  semesterIndex,
+  tableSize,
+  cellWidth,
+  cellHeight,
+  isPlannerWithSummer,
+  isPlannerWithWinter,
+  onClick,
+}) => {
+  const handleClick = onClick
+    ? (event) => {
+        onClick();
+      }
+    : null;
+
+  const getTop = () => {
+    const base = 17 + (isPlannerWithSummer ? 15 : 0) + cellHeight * tableSize;
+    if (semesterIndex === 0) {
+      return base - cellHeight * tableSize + 2;
+    }
+    if (semesterIndex === 1) {
+      return base + cellHeight * 2 + 11 + 1;
+    }
+    return base;
+  };
+
+  return (
+    <div
+      className={classNames('planner-overlay')}
+      style={{
+        left: 26 + (cellWidth + 15) * yearIndex - 1,
+        top: getTop(),
+        width: cellWidth + 2,
+        height: cellHeight * tableSize - 3,
+      }}
+      onClick={handleClick}>
+      <div className={classNames('planner-overlay__button')}>
+        {`${getSemesterName(semesterIndex * 2 + 1)}학기에 추가하기`}
+      </div>
+    </div>
+  );
+};
+
+PlannerOverlay.propTypes = {
+  yearIndex: PropTypes.number.isRequired,
+  semesterIndex: PropTypes.oneOf([0, 1]).isRequired,
+  tableSize: PropTypes.number.isRequired,
+  cellWidth: PropTypes.number.isRequired,
+  cellHeight: PropTypes.number.isRequired,
+  isPlannerWithSummer: PropTypes.bool.isRequired,
+  isPlannerWithWinter: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default withTranslation()(React.memo(PlannerOverlay));
