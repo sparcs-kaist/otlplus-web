91c212a00c36838c3dae95d5e1c80103d954e539
Jiho Park <jihopark7777@gmail.com>
2023-08-24 02:32:54 +0900

    fix: fix styling issues

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production' ? 'https://otl-dev.sparcs.org' : 'http://localhost:58000';
+export const API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:58000';
