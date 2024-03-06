13f0dd2ebbc9a9f0620d297ba6f88c0ae7d75ac1
useruseruse <eliza1102@kaist.ac.kr>
2024-03-06 22:00:16 +0900

    migrate planner/arbitary planner item

---
+++ src/shapes/model/planner/arbitraryPlannerItem.tsx
@@ -0,0 +1,16 @@
+import department from '../subject/department';
+
+interface arbitraryPlannerItem {
+  id: number;
+  item_type: 'ARBITRARY';
+  is_excluded: boolean;
+  year: number;
+  semester: 1 | 2 | 3 | 4;
+  department?: department;
+  type: string;
+  type_en: string;
+  credit: number;
+  credit_au: number;
+}
+
+export default arbitraryPlannerItem;
