ef29dd468ed66b434f89f8571fac5df4c81696dd
YINGMEI <j.yeongmee@gmail.com>
2024-03-06 22:37:23 +0900

    feat: update graduation related shapes to ts interface

---
+++ src/shapes/model/graduation/additionalTrack.tsx
@@ -0,0 +1,20 @@
+import department from '../subject/department';
+
+export enum AdditionalTrackType {
+  DOUBLE,
+  MINOR,
+  ADVANCED,
+  INTERDISCIPLINARY,
+}
+
+interface additionalTrack {
+  id: number;
+  start_year: number;
+  end_year: number;
+  type: AdditionalTrackType;
+  department: department;
+  major_required: number;
+  major_elective: number;
+}
+
+export default additionalTrack;
