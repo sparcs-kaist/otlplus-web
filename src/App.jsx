7c83800df1b820d7b28137647cc9bff2f7b745b6
Seungbin Oh <sboh1214@gmail.com>
2024-03-11 17:55:32 +0900

    chore: show redux devtools extension only in dev

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,9 @@ const store = createStore(
     writeReviews: writeReviewsReducer,
     planner: plannerReducer,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null,
 );
 
 class App extends Component {
