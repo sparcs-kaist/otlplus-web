9ed5912507f505e41f89e5e9f271f9235a4cedee
Yumin Cho <dbals081216@gmail.com>
2024-03-12 19:17:25 +0900

    migrate: /shapes/state/write-reviews

---
+++ src/shapes/state/write-reviews/ReviewsFocus.ts
@@ -0,0 +1,30 @@
+import { ReviewsFocusFrom } from '@/shapes/enum';
+
+import Lecture from '@/shapes/model/subject/Lecture';
+import Review from '@/shapes/model/review/Review';
+
+interface FromNone {
+  from: ReviewsFocusFrom.NONE;
+  lecture?: null;
+  reviews?: null;
+}
+
+interface FromLecture {
+  from: ReviewsFocusFrom.LECTURE;
+  lecture: Lecture;
+  reviews: Review[];
+}
+
+interface FromReviews {
+  from:
+    | ReviewsFocusFrom.REVIEWS_LATEST
+    | ReviewsFocusFrom.REVIEWS_MY
+    | ReviewsFocusFrom.REVIEWS_LIKED
+    | ReviewsFocusFrom.REVIEWS_RANKED;
+  lecture?: null;
+  reviews: Review[];
+}
+
+type ReviewsFocus = FromNone | FromLecture | FromReviews;
+
+export default ReviewsFocus;
