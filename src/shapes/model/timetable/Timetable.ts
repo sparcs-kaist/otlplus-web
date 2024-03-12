abf90792163d7bce3ef9290e5068dc40716e6e45
Yumin Cho <dbals081216@gmail.com>
2024-03-12 16:50:34 +0900

    migrate: /shapes/model/timetable

---
+++ src/shapes/model/timetable/Timetable.ts
@@ -0,0 +1,13 @@
+import Lecture from '../subject/Lecture';
+
+export interface MyPseudoTimetable {
+  id: number;
+  lectures: Lecture[];
+  isReadOnly: true;
+}
+
+export default interface Timetable {
+  id: number;
+  lectures: Lecture[];
+  arrange_order: number;
+}
