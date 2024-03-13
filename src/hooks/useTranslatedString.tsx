0c6877168d70f6bd4325794ca6ec73869676bbcf
Yumin Cho <dbals081216@gmail.com>
2024-03-14 00:29:25 +0900

    docs: add docstring for useTranslatedString hook

--- src/hooks/useTranslatedString.tsx
+++ src/hooks/useTranslatedString.tsx
@@ -1,5 +1,12 @@
 import { useTranslation } from 'react-i18next';
 
+/**
+ * Custom hook that returns a function for translating strings based on the current language.
+ *
+ * @returns {function} A function that takes an object and a key and returns the translated string.
+ * @example const translate = useTranslatedString();
+ *          const translatedString = translate(review.lecture, 'title');
+ */
 export const useTranslatedString = () => {
   const { i18n } = useTranslation();
 
