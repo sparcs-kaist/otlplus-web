595597b7f4d3f0cdfc3e5c29e46256a2612dcbd3
Yumin Cho <dbals081216@gmail.com>
2024-03-17 20:10:02 +0900

    migrate: @/components/blocks/CourseSimpleBlock.tsx

--- src/components/blocks/CourseSimpleBlock.tsx
+++ src/components/blocks/CourseSimpleBlock.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-
 import { appBoundClassNames as classNames } from '../../common/boundClassNames';
+import Course from '@/shapes/model/subject/Course';
+import { useTranslatedString } from '@/hooks/useTranslatedString';
 
-import courseShape from '../../shapes/model/subject/CourseShape';
+interface Props {
+  course: Course;
+}
 
-const CourseSimpleBlock = ({ t, course }) => {
+const CourseSimpleBlock: React.FC<Props> = ({ course }) => {
+  const translate = useTranslatedString();
+
   return (
     <div className={classNames('block', 'block--course-simple')}>
-      <div className={classNames('block--course-simple__title')}>
-        {course[t('js.property.title')]}
-      </div>
+      <div className={classNames('block--course-simple__title')}>{translate(course, 'title')}</div>
       <div className={classNames('block--course-simple__subtitle')}>{course.old_code}</div>
     </div>
   );
-};
-
-CourseSimpleBlock.propTypes = {
-  course: courseShape.isRequired,
 };
 
 export default withTranslation()(React.memo(CourseSimpleBlock));
