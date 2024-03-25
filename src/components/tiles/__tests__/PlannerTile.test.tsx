0d42c327ccd15072beb9812316e2951a64bb386d
Seungbin Oh <sboh1214@gmail.com>
2024-03-26 03:38:23 +0900

    test: add test for tiles

--- src/components/tiles/__tests__/PlannerTile.test.tsx
+++ src/components/tiles/__tests__/PlannerTile.test.tsx
@@ -1,87 +1,16 @@
-import { render } from '@/test-utils';
+import { render, sampleCourse, sampleLecture } from '@/test-utils';
 import PlannerTile from '../PlannerTile';
-import { PlannerItemType, SemesterType } from '@/shapes/enum';
+import { PlannerItemType } from '@/shapes/enum';
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => {
-    return {
-      t: (str: string) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-        language: 'ko',
-      },
-    };
-  },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  withTranslation: () => (Component: any) => {
-    Component.defaultProps = { ...Component.defaultProps, t: () => '' };
-    return Component;
-  },
-}));
-
-test('loads and displays greeting', async () => {
+test('renders PlannerTile', async () => {
   render(
     <PlannerTile
       item={{
         id: 0,
         item_type: PlannerItemType.TAKEN,
         is_excluded: true,
-        lecture: {
-          id: 0,
-          title: '',
-          title_en: '',
-          course: 0,
-          old_code: '',
-          class_no: '',
-          year: 0,
-          semester: SemesterType.SPRING,
-          code: '',
-          department: 0,
-          department_code: '',
-          department_name: '',
-          department_name_en: '',
-          type: '',
-          type_en: '',
-          limit: 0,
-          num_people: 0,
-          is_english: false,
-          num_classes: 0,
-          num_labs: 0,
-          credit: 0,
-          credit_au: 0,
-          common_title: '',
-          common_title_en: '',
-          class_title: '',
-          class_title_en: '',
-          review_total_weight: 0,
-          professors: [],
-          grade: 0,
-          load: 0,
-          speech: 0,
-          classtimes: [],
-          examtimes: [],
-        },
-        course: {
-          id: 0,
-          old_code: '',
-          type: '',
-          type_en: '',
-          title: '',
-          title_en: '',
-          summary: '',
-          review_total_weight: 0,
-          credit: 0,
-          credit_au: 0,
-          num_classes: 0,
-          num_labs: 0,
-          related_courses_prior: [],
-          related_courses_posterior: [],
-          professors: [],
-          grade: 0,
-          load: 0,
-          speech: 0,
-          userspecific_is_read: false,
-        },
+        lecture: sampleLecture,
+        course: sampleCourse,
       }}
       yearIndex={0}
       semesterIndex={0}
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,5 +1,9 @@
 import React, { ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
+import Course from '@/shapes/model/subject/Course';
+import { SemesterType } from '@/shapes/enum';
+import Lecture from '@/shapes/model/subject/Lecture';
+import Classtime from '@/shapes/model/subject/Classtime';
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return children;
@@ -10,3 +14,73 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>
 
 export * from '@testing-library/react';
 export { customRender as render };
+
+export const sampleCourse: Course = {
+  id: 0,
+  old_code: '',
+  type: '',
+  type_en: '',
+  title: '',
+  title_en: '',
+  summary: '',
+  review_total_weight: 0,
+  credit: 0,
+  credit_au: 0,
+  num_classes: 0,
+  num_labs: 0,
+  related_courses_prior: [],
+  related_courses_posterior: [],
+  professors: [],
+  grade: 0,
+  load: 0,
+  speech: 0,
+  userspecific_is_read: false,
+};
+
+export const sampleLecture: Lecture = {
+  id: 0,
+  title: '',
+  title_en: '',
+  course: 0,
+  old_code: '',
+  class_no: '',
+  year: 0,
+  semester: SemesterType.SPRING,
+  code: '',
+  department: 0,
+  department_code: '',
+  department_name: '',
+  department_name_en: '',
+  type: '',
+  type_en: '',
+  limit: 0,
+  num_people: 0,
+  is_english: false,
+  num_classes: 0,
+  num_labs: 0,
+  credit: 0,
+  credit_au: 0,
+  common_title: '',
+  common_title_en: '',
+  class_title: '',
+  class_title_en: '',
+  review_total_weight: 0,
+  professors: [],
+  grade: 0,
+  load: 0,
+  speech: 0,
+  classtimes: [],
+  examtimes: [],
+};
+
+export const sampleClasstime: Classtime = {
+  building_code: '',
+  classroom: '',
+  classroom_en: '',
+  classroom_short: '',
+  classroom_short_en: '',
+  room_name: '',
+  day: 0,
+  begin: 0,
+  end: 0,
+};
