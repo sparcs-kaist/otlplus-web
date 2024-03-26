7f8a79404dae80259a9ece66f3647cbded979084
Yumin Cho <dbals081216@gmail.com>
2024-03-26 17:50:44 +0900

    migrate: @/actions/common

---
+++ src/actions/common/track.ts
@@ -0,0 +1,17 @@
+// const BASE_STRING = 'C_T_';
+
+/* eslint-disable prefer-template */
+export const SET_TRACKS = 'SET_TRACKS' as const;
+/* eslint-enable prefer-template */
+
+import GeneralTrack from '@/shapes/model/graduation/GeneralTrack';
+import AdditionalTrack from '@/shapes/model/graduation/AdditionalTrack';
+
+export function setTracks(tracks: Array<GeneralTrack | AdditionalTrack>) {
+  return {
+    type: SET_TRACKS,
+    tracks: tracks,
+  };
+}
+
+export type TrackAction = ReturnType<typeof setTracks>;
