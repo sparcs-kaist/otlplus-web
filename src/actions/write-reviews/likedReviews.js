ca48c57ddf5b71fd0d6614e2569501bcebd97c65
jihyeon <eliza1102@kaist.ac.kr>
2024-03-27 13:20:30 +0000

    fix bug

--- src/actions/write-reviews/likedReviews.js
+++ src/actions/write-reviews/likedReviews.js
@@ -3,7 +3,6 @@ const BASE_STRING = 'WR_LkR_';
 /* eslint-disable prefer-template */
 export const RESET = BASE_STRING + 'RESET';
 export const SET_REVIEWS = BASE_STRING + 'SET_REVIEWS';
-export const UPDATE_REVIEW = BASE_STRING + 'UPDATE_REVIEW';
 /* eslint-enable prefer-template */
 
 export function reset() {
@@ -19,10 +18,3 @@ export function setReviews(reviews) {
   };
 }
 
-export function updateReview(review, isNew) {
-  return {
-    type: UPDATE_REVIEW,
-    review: review,
-    isNew: isNew,
-  };
-}
