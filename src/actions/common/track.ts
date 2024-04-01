373e8c8febe89852866b0c9c4f607e50e9af9968
Yumin Cho <dbals081216@gmail.com>
2024-04-02 00:56:40 +0900

    fix: fix wrong action input type

--- src/actions/common/track.ts
+++ src/actions/common/track.ts
@@ -5,9 +5,16 @@ export const SET_TRACKS = `${BASE_STRING}SET_TRACKS` as const;
 /* eslint-enable prefer-template */
 
 import GeneralTrack from '@/shapes/model/graduation/GeneralTrack';
+import MajorTrack from '@/shapes/model/graduation/MajorTrack';
 import AdditionalTrack from '@/shapes/model/graduation/AdditionalTrack';
 
-export function setTracks(tracks: Array<GeneralTrack | AdditionalTrack>) {
+interface TracksProps {
+  general: GeneralTrack[];
+  major: MajorTrack[];
+  additional: AdditionalTrack[];
+}
+
+export function setTracks(tracks: TracksProps) {
   return {
     type: SET_TRACKS,
     tracks: tracks,
