62bb76084cce10aa6303a4685d5127a0502fc9cd
Yumin Cho <dbals081216@gmail.com>
2024-04-02 00:48:08 +0900

    fix: remove unused updateReview action

--- src/actions/write-reviews/likedReviews.ts
+++ src/actions/write-reviews/likedReviews.ts
@@ -5,7 +5,6 @@ const BASE_STRING = 'WR_LkR_';
 /* eslint-disable prefer-template */
 export const RESET = `${BASE_STRING}RESET` as const;
 export const SET_REVIEWS = `${BASE_STRING}SET_REVIEWS` as const;
-export const UPDATE_REVIEW = `${BASE_STRING}UPDATE_REVIEW` as const;
 /* eslint-enable prefer-template */
 
 export function reset() {
@@ -21,15 +20,4 @@ export function setReviews(reviews: Review[]) {
   };
 }
 
-export function updateReview(review: Review, isNew: boolean) {
-  return {
-    type: UPDATE_REVIEW,
-    review: review,
-    isNew: isNew,
-  };
-}
-
-export type LikedReviewsAction =
-  | ReturnType<typeof reset>
-  | ReturnType<typeof setReviews>
-  | ReturnType<typeof updateReview>;
+export type LikedReviewsAction = ReturnType<typeof reset> | ReturnType<typeof setReviews>;
