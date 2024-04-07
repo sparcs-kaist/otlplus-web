db096ed93bb1825866e10d8c90441e32bf6ccf74
Yumin Cho <dbals081216@gmail.com>
2024-04-07 15:44:40 +0900

    migrate: @/reducers/timetable/semester.ts

---
+++ src/reducers/timetable/semester.ts
@@ -0,0 +1,31 @@
+import { RESET, SET_SEMESTER, SemesterAction } from '@/actions/timetable/semester';
+import { SemesterType } from '@/shapes/enum';
+
+interface SemesterState {
+  year: number | null;
+  semester: SemesterType | null;
+}
+
+const initialState: SemesterState = {
+  year: null,
+  semester: null,
+};
+
+const semester = (state = initialState, action: SemesterAction) => {
+  switch (action.type) {
+    case RESET: {
+      return initialState;
+    }
+    case SET_SEMESTER: {
+      return {
+        year: action.year,
+        semester: action.semester,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default semester;
