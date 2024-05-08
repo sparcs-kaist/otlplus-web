0f9c7538d337e042563808c5787e4b900608f76b
david <dongyun556@gmail.com>
2024-05-08 19:48:35 +0900

    migrate: migrate common reducers

--- src/reducers/common/user.ts
+++ src/reducers/common/user.ts
@@ -1,17 +1,23 @@
-import { SET_USER, UPDATE_USER_REVIEW } from '../../actions/common/user';
+import User from '@/shapes/model/session/User';
+import { SET_USER, UPDATE_USER_REVIEW, UserAction } from '../../actions/common/user';
 
-const initialState = {
+interface UserState {
+  user?: User;
+}
+
+const initialState: UserState = {
   user: undefined,
 };
 
-export const reducer = (state = initialState, action) => {
+export const reducer = (state = initialState, action: UserAction): UserState => {
   switch (action.type) {
     case SET_USER:
-      return Object.assign({}, state, {
-        user: action.user,
-      });
+      return { ...state, user: action.user };
     case UPDATE_USER_REVIEW: {
-      const originalReviews = state.user.reviews;
+      if (state.user === undefined) {
+        return state;
+      }
+      const originalReviews = state.user?.reviews || [];
       const { review } = action;
       const foundIndex = originalReviews.findIndex((r) => r.id === review.id);
       const newReviews =
@@ -22,12 +28,7 @@ export const reducer = (state = initialState, action) => {
               ...originalReviews.slice(foundIndex + 1, originalReviews.length),
             ]
           : [...originalReviews.slice(), review];
-      return Object.assign({}, state, {
-        user: {
-          ...state.user,
-          reviews: newReviews,
-        },
-      });
+      return { ...state, user: { reviews: newReviews } };
     }
     default:
       return state;
