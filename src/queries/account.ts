0ce05c16e64f48c83cfa8489dde8397d6417d6e8
Yumin Cho <dbals081216@gmail.com>
2024-05-16 20:29:10 +0900

    feat: add queries for account page

---
+++ src/queries/account.ts
@@ -0,0 +1,44 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useSessionInfo = () => {
+  return useQuery({
+    queryKey: ['sessionInfo'],
+    staleTime: Infinity,
+    queryFn: async () => {
+      return (
+        await axios.get('/session/info', {
+          metadata: {
+            gaCategory: 'User',
+            gaVariable: 'GET / Instance',
+          },
+        })
+      ).data;
+    },
+  });
+};
+
+export const useDepartmentOptions = () => {
+  return useQuery({
+    queryKey: ['departmentOptions'],
+    staleTime: Infinity,
+    queryFn: async () => {
+      return (await axios.get('/session/department-options')).data;
+    },
+  });
+};
+
+export const useUpdateFavoriteDepartments = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ selectedDepartments }: { selectedDepartments: string[] }) => {
+      return axios.post('/session/favorite-departments', {
+        fav_department: Array.from(selectedDepartments).filter((d) => d !== 'ALL'),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['departmentOptions'] });
+      queryClient.invalidateQueries({ queryKey: ['sessionInfo'] });
+    },
+  });
+};
